cdfdcd350a53b6d5570e405115b9e65e4f595564
mcerdeno2021 <m.cerdeno.2021@alumnos.urjc.es>
2025-10-03 18:35:42 +0200

    Inicio Apartado 3 y Readme

--- 3-Modelo_Mensajes_Tiempo_Virtual_Historia/historia.js
+++ 3-Modelo_Mensajes_Tiempo_Virtual_Historia/historia.js
@@ -6,12 +6,32 @@ AFRAME.registerComponent('historia', {
 
       el.addEventListener('historia', e => {
         console.log(e.detail);
-        historia[e.detail[0]] = {
-          indice : e.detail[1],
-          posicionOrigen: e.detail[2],
-          posicionDestino: e.detail[3],
-          huella: e.detail[4]
+        historia = {
+          posicionOrigen: e.detail[0],
+          posicionDestino: e.detail[1],
+          tiempoOrigen: e.detail[2],
+          tiempoDestino: e.detail[3]
         };
+
+        const movimientos = historia.tiempoDestino - historia.tiempoOrigen;
+        const posicionesX = [];
+        const posicionesY = [];
+        const posicionesZ = [];
+
+        for (let i = 0; i <= movimientos; i++) {
+          const x = historia.posicionOrigen.x + ((historia.posicionDestino.x - historia.posicionOrigen.x) / movimientos) * i;
+          const y = historia.posicionOrigen.y + ((historia.posicionDestino.y - historia.posicionOrigen.y) / movimientos) * i;
+          const z = historia.posicionOrigen.z + ((historia.posicionDestino.z - historia.posicionOrigen.z) / movimientos) * i;
+          posicionesX.push(x);
+          posicionesY.push(y);
+          posicionesZ.push(z);
+        }
+      })
+      
+      el.addEventListener('reloj-tick', e => {
+        if (e.detail >= historia.tiempoOrigen && e.detail <= historia.tiempoDestino) {
+          el.emit('mensaje', posicionesX, posicionesY, posicionesZ); 
+        }
       })
     }
 })
